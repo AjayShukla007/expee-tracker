1f664e735f5e36d86cc47731fe25496de1763976
AjayShukla007 <shuklaajay0838@gmail.com>
2024-04-15 19:01:21 +0530

    Added addtoCard button

---
+++ app/components/AddToCart.tsx
@@ -0,0 +1,14 @@
+'use client'
+
+import React from 'react'
+
+const AddToCart = () => {
+    return (
+        <div>
+            <button onClick={() => console.log('add to card clicked')
+            }>ADDTOCART</button>
+        </div>
+    )
+}
+
+export default AddToCart
