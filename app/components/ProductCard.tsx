35e53a60e58949ac5e9dbd3a9a0b3a4573400209
AjayShukla007 <shuklaajay0838@gmail.com>
2024-04-16 19:57:09 +0530

    Initial commit

--- app/components/ProductCard.tsx
+++ app/components/ProductCard.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import AddToCart from './AddToCart'
 // import styles from './ProductCart.module.css';
 
-
 const ProductCard = () => {
   return (
     <div className='p-5 my-5 bg-sky-500 text-white text-xl hover:bg-sky-600'>
