87c4b8723c2c95a3dda26b46db43870046130870
AjayShukla007 <shuklaajay0838@gmail.com>
2024-04-18 21:02:27 +0530

    Uodated main input and added state for he same

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,27 @@
 'use client'
 import Image from 'next/image'
-
 import React, { useState, useEffect } from 'react'
-
 import Link from 'next/link'
+
+import { collection, addDoc } from "firebase/firestore";
 
 interface Items {
   name: string,
   price: number
 }
+
+// Add items to the db
+const addItems = async (e:any) => {
+  e.preventDefault();
+
+}
+
+
+// Read items from the db 
+
+// update items from the db
+
+// delete items from the db
 
 export default function Home() {
 
@@ -17,6 +30,11 @@ export default function Home() {
     { name: 'movie', price: 20.00 },
     { name: 'kurkure', price: 5.00 }
   ])
+
+  const [newItem, setNewItem] = useState<Items>({
+    name: '',
+    price: 0
+  })
   const [total, setTotal] = useState(0)
 
   return (
@@ -25,27 +43,27 @@ export default function Home() {
       <Link href="/about">About</Link>
       <div className='bg-slate-800 p-4 rounded-lg'>
         <form className='grid grid-cols-6 items-center text-black'>
-          <input className='col-span-3 p-3 border ' type="text" placeholder='Enter item' />
-          <input className='col-span-2 p-3 border mx-3' type="number" placeholder='Enter $' />
+          <input onChange={(e:any)=> setNewItem({...newItem, name: e.target.value})} value={newItem.name} className='col-span-3 p-3 border ' type="text" placeholder='Enter item' />
+          <input onChange={(e:any)=> setNewItem({...newItem, price: e.target.value})} value={newItem.price} className='col-span-2 p-3 border mx-3' type="number" placeholder='Enter $' />
           <button className='text-white bg-slate-950 hover:bg-slate-900 p-3 text-xl' type='submit'>+</button>
         </form>
         <ul>
           {items.map((item, id) => (
             <li key={id} className='bg-slate-900 my-4 flex justify-between'>
               <div className='p-4 w-full flex justify-between'>
-              {item.name} - ${item.price}
+                {item.name} - ${item.price}
 
               </div>
               <button className='ml-4 p-4 border-l-2 border-slate-900 hover:bg-slate-950 w-16'>X</button>
             </li>
           ))}
         </ul>
-          {items.length < 1 ? ('') : (
-            <div className='flex justify-between p-3'>
-              <span>Total</span>
-              <span>${total}</span>
-            </div>
-          )}
+        {items.length < 1 ? ('') : (
+          <div className='flex justify-between p-3'>
+            <span>Total</span>
+            <span>${total}</span>
+          </div>
+        )}
       </div>
     </main>
   )
