dc5eb51e363045d1a27412268bddb5951287695f
AjayShukla007 <shuklaajay0838@gmail.com>
2024-04-19 20:06:20 +0530

    Working on post route for expance items

--- app/page.tsx
+++ app/page.tsx
@@ -10,18 +10,6 @@ interface Items {
   price: number
 }
 
-// Add items to the db
-const addItems = async (e:any) => {
-  e.preventDefault();
-
-}
-
-
-// Read items from the db 
-
-// update items from the db
-
-// delete items from the db
 
 export default function Home() {
 
@@ -36,15 +24,34 @@ export default function Home() {
     price: 0
   })
   const [total, setTotal] = useState(0)
+  const [warning, setWarning] = useState<string>('')
+  // Add items to the db
+  const addItems = async (e: any) => {
+    e.preventDefault();
+    
+    if (newItem.name === '' || newItem.price === 0){
+      setWarning("PLEASE ENTER VAlid INPUT")
+      return;
+    }
+}
+
+
+  // Read items from the db 
+
+  // update items from the db
+
+  // delete items from the db
+
 
   return (
     <main className='flex min-h-screen flex-col items-center justify-start p-24'>
+      <p className='text-red-700'>{warning}</p>
       <h1 className='text-4xl p-4'>Expence Tracker</h1>
       <Link href="/about">About</Link>
       <div className='bg-slate-800 p-4 rounded-lg'>
         <form className='grid grid-cols-6 items-center text-black'>
-          <input onChange={(e:any)=> setNewItem({...newItem, name: e.target.value})} value={newItem.name} className='col-span-3 p-3 border ' type="text" placeholder='Enter item' />
-          <input onChange={(e:any)=> setNewItem({...newItem, price: e.target.value})} value={newItem.price} className='col-span-2 p-3 border mx-3' type="number" placeholder='Enter $' />
+          <input onChange={(e: any) => setNewItem({ ...newItem, name: e.target.value })} value={newItem.name} className='col-span-3 p-3 border ' type="text" placeholder='Enter item' />
+          <input onChange={(e: any) => setNewItem({ ...newItem, price: e.target.value })} value={newItem.price} className='col-span-2 p-3 border mx-3' type="number" placeholder='Enter $' />
           <button className='text-white bg-slate-950 hover:bg-slate-900 p-3 text-xl' type='submit'>+</button>
         </form>
         <ul>
@@ -54,7 +61,7 @@ export default function Home() {
                 {item.name} - ${item.price}
 
               </div>
-              <button className='ml-4 p-4 border-l-2 border-slate-900 hover:bg-slate-950 w-16'>X</button>
+              <button onClick={addItems} className='ml-4 p-4 border-l-2 border-slate-900 hover:bg-slate-950 w-16'>X</button>
             </li>
           ))}
         </ul>
